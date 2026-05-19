f73f46392e09b623fba669584702f1f6a279f89e
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2026-05-19 20:49:16 +0700

    feat: replace discord with tiktok in admin settings and footer social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,10 +14,10 @@ const TelegramIcon = ({ className }) => (
     </svg>
 );
 
-// Discord SVG icon
-const DiscordIcon = ({ className }) => (
-    <svg className={className} viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057c.002.022.015.043.03.056a19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028 14.09 14.09 0 0 0 1.226-1.994.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z"/>
+// TikTok SVG icon
+const TikTokIcon = ({ className }) => (
+    <svg className={className} viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
     </svg>
 );
 
@@ -30,7 +30,7 @@ const Footer = () => {
         facebook: '',
         instagram: '',
         telegram: '',
-        discord: ''
+        tiktok: ''
     });
 
     useEffect(() => {
@@ -76,7 +76,7 @@ const Footer = () => {
                                 { icon: Facebook, color: 'hover:text-blue-400 hover:border-blue-400/50', url: settings.facebook, label: 'Facebook' },
                                 { icon: Instagram, color: 'hover:text-pink-400 hover:border-pink-400/50', url: settings.instagram, label: 'Instagram' },
                                 { icon: TelegramIcon, color: 'hover:text-sky-400 hover:border-sky-400/50', url: settings.telegram, label: 'Telegram' },
-                                { icon: DiscordIcon, color: 'hover:text-indigo-400 hover:border-indigo-400/50', url: settings.discord, label: 'Discord' }
+                                { icon: TikTokIcon, color: 'hover:text-pink-500 hover:border-pink-500/50', url: settings.tiktok, label: 'TikTok' }
                             ].map((social, i) => (
                                 social.url ? (
                                     <a key={i} href={social.url} target="_blank" rel="noopener noreferrer" aria-label={social.label}
